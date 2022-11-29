e1dfc92a1daab051133ae6195dc67f51ccd6c75a
StormCrozier <stormcrozier950@gmail.com>
2022-11-29 15:15:40 -0800

    Merge of Voice and Chat -io

--- public/js/voice.js
+++ public/js/voice.js
@@ -1,57 +1,57 @@
-//const socket = io();
+// const socket = io();
 const peer = new Peer();
-//Voice Chat Div 
+// Voice Chat Div 
 const voiceApp = document.getElementById('voiceApp');
-//Names of users in Chat
+// Names of users in Chat
 const audioGrid = document.getElementById("inVoice");
-//Clients audio
+// Clients audio
 const myAudio = document.createElement("audio");
-//Sound to play on user join
+// Sound to play on user join
 const joinSound = document.getElementById('onJoin')
-//Room Id to connect to
+// Room Id to connect to
 const roomToConnect = 'voice-chat';
-//Clients Nick name
+// Clients Nick name
 let myNickName = '';
-//Users in voice Chat nicknames
+// Users in voice Chat nicknames
 let thierName = [];
-//Mute clients audio
+// Mute clients audio
 myAudio.muted = true;
 
-//List of calls used to end calls between users
-let peers = {};
+// List of calls used to end calls between users
+const peers = {};
 
-//Pop up to get user nick name
-window.addEventListener("load", function () {
-  setTimeout(function open(event) {
+// Pop up to get user nick name
+window.addEventListener("load", () => {
+  setTimeout((event) => {
     document.querySelector(".popup").style.display = "block";
   }, 1000);
 });
-//Close pop up event
-document.querySelector("#close").addEventListener("click", function () {
+// Close pop up event
+document.querySelector("#close").addEventListener("click", () => {
   document.querySelector(".popup").style.display = "none";
   myNickName = document.getElementById("userName").value;
 });
 
-//Join Room function
-//Puts client in room and alerts other users in voice of new user
-//Uses in room call the person joining
+// Join Room function
+// Puts client in room and alerts other users in voice of new user
+// Uses in room call the person joining
 const joinRoom = () => {
-  let getUserMedia =
+  const getUserMedia =
     navigator.getUserMedia ||
     navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia;
 
-  //Puts client in room
+  // Puts client in room
   socket.emit("join-room", roomToConnect, peer.id, myNickName);
 
   voiceApp.style.display = 'block';
 
-  //Gets client audio 
+  // Gets client audio 
   getUserMedia({ video: false, audio: true }, (mediaStream) => {
     myStream = mediaStream;
     let count = -1;
     addAudioStream(myAudio, myNickName, 'myAudio', mediaStream);
-    //When user recives a call
+    // When user recives a call
     peer.off("call").on("call", (call, userId) => {
       console.log(thierName);
       call.answer(mediaStream);
@@ -65,34 +65,33 @@ const joinRoom = () => {
         addAudioStream(audio, thierName[count], call.peer, userAudioStream);
       });
     });
-    //Calls connectNewUser Function and alerts users in room of new user
+    // Calls connectNewUser Function and alerts users in room of new user
     socket.off("new-user-connected").on("new-user-connected", (userId, nickName) => {
-      let type = document.querySelectorAll("#systemMessages");
+      const type = document.querySelectorAll("#systemMessagesChat");
       console.log(type);
-      type[1].innerHTML = `<marquee>User ${nickName} joined</marquee>`;
-      type[1].style.visibility = "visible";
-      type[1].style.opacity = 1;
+      type[0].innerHTML = `<marquee>User ${nickName} joined</marquee>`;
+      type[0].style.visibility = "visible";
+      type[0].style.opacity = 1;
       console.log(joinSound);
       joinSound.play();
       socket.emit("added-to-voice", peer.id);
-      console.log("New user: " + userId);
+      console.log(`New user: ${  userId}`);
       setTimeout(connectNewUser, 1000, userId, nickName, mediaStream);
     });
-    //Alerting everyone else in the room of new user
+    // Alerting everyone else in the room of new user
     socket.emit("connection-request", roomToConnect, peer.id, myNickName);
 
     peer.on("error", (err) => console.log(err));
   });
 };
 
-//Function to leave voice chat room
-//Removes client from room and lets other users know of user leaving
+// Function to leave voice chat room
+// Removes client from room and lets other users know of user leaving
 const leaveRoom = () => {
-  voiceApp.style.display = 'none' 
   thierName = [];
-  //Leaves Room
+  // Leaves Room
   socket.emit("leave-room", roomToConnect);
-  //Alert other users to who left
+  // Alert other users to who left
   socket.emit("user-left", roomToConnect, peer.id, myNickName);
 
   /*
@@ -101,22 +100,22 @@ const leaveRoom = () => {
       track.stop();
     });
     */
-  //Removes audi divs from html
+  // Removes audi divs from html
   removeUnusedDivs();
   myAudio.muted = true;
   myAudio.srcObject = null;
 };
 
-//Sets thierName to users currently in room
+// Sets thierName to users currently in room
 socket.on('give-nick', (nickName) => {thierName.push(nickName)});
 
-//Alerts other users of user leaving
+// Alerts other users of user leaving
 socket.on('user-left', (userId, nickName) => {
-  let type = document.querySelectorAll("#systemMessages");
+  const type = document.querySelectorAll("#systemMessagesChat");
   console.log(type);
-  type[1].innerHTML = `<marquee>User ${nickName} left</marquee>`;
-  type[1].style.visibility = "visible";
-  type[1].style.opacity = 1;
+  type[0].innerHTML = `<marquee>User ${nickName} left</marquee>`;
+  type[0].style.visibility = "visible";
+  type[0].style.opacity = 1;
   const toRemove = document.getElementById(userId);
   toRemove.remove();
   if (peers[userId]){
@@ -125,9 +124,9 @@ socket.on('user-left', (userId, nickName) => {
   } 
 });
 
-//Connects to a new user
-//Calls user joining room 
-//Adds call object to peers
+// Connects to a new user
+// Calls user joining room 
+// Adds call object to peers
 const connectNewUser = (userId, nickName, stream) => {
   socket.emit('give-nick', roomToConnect, myNickName)
   const call = peer.call(userId, stream);
@@ -145,7 +144,7 @@ const connectNewUser = (userId, nickName, stream) => {
   console.log(peers);
 };
 
-//Adds audio, nickname, and mute button to DOM
+// Adds audio, nickname, and mute button to DOM
 const addAudioStream = (audio, nickName, id, stream) => {
   const div = document.createElement('div');
   const name = document.createElement('h1');
@@ -167,7 +166,7 @@ const addAudioStream = (audio, nickName, id, stream) => {
   audioGrid.appendChild(div);
 };
 
-//Removes audio divs when a user leaves the chat
+// Removes audio divs when a user leaves the chat
 const removeUnusedDivs = () => {
   alldivs = audioGrid.getElementsByTagName("div"); 
   while (audioGrid.hasChildNodes) {
@@ -175,7 +174,7 @@ const removeUnusedDivs = () => {
   }
 };
 
-//Function to mute clients audio
+// Function to mute clients audio
 const muteMyAudio = () => {
   const audi = myStream.getAudioTracks();
   audi[0].enabled = !audi[0].enabled;
@@ -188,7 +187,7 @@ const muteMyAudio = () => {
     myName.style.color = 'red';
 }
 
-//Mutes other users audio on button click
+// Mutes other users audio on button click
 const muteThierAudio = (e) => {
   const div = e.target.parentElement;
   const toMute = div.querySelector('audio');
@@ -200,7 +199,7 @@ const muteThierAudio = (e) => {
     toChange.style.color = 'black';
 }
 
-//Adds mute button to audio grid for each user in chat
+// Adds mute button to audio grid for each user in chat
 const addMuteButton = () => {
   const but = document.createElement('button');
   but.setAttribute('type', 'button');
