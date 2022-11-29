c7c417d8359b57cdb355cdac1fc50a730dc41e3b
Travis Block <t.block@pdx.edu>
2022-11-29 00:22:06 -0800

    added icons, apps, sanitization, styles.

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
-const http = require('http');
+const http = require("http");
+
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+
 const io = new Server(server);
-const chatFunc = require('./private/chatFunctions' )
+const chatFunc = require("./private/chatFunctions");
 
-const port = process.env.PORT || 5002;
+const port = server.port || 5002;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use('/', (req, res) => {
-  res.sendFile("index.html",{"root":"public"});
+app.use("/", (req, res) => {
+  res.sendFile("index.html", { root: "public" });
 });
 
-io.on('connection', (socket) => {
-
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', chatFunc.parseMessage(msg));
-      console.log('message: ' + msg);
-    });
-    
-    socket.on('typing', ()  => {
-        io.emit('typing');
-    });
-
-    socket.on('hidebox', () => {
-        io.emit('hidebox');
-    });
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", chatFunc.parseMessage(msg));
+    console.log(`message: ${msg}`);
   });
+
+  socket.on("typing", () => {
+    io.emit("typing");
+  });
+
+  socket.on("hidebox", () => {
+    io.emit("hidebox");
+  });
+});
 
 server.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}`);
